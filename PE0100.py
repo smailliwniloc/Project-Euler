25d0b638b2eca722068390ed729ab72d3ae1b9e6
Colin <smailliwniloc@gmail.com>
2022-09-21 08:50:14 -0500

    Added link

--- PE0100.py
+++ PE0100.py
@@ -1,5 +1,7 @@
 # PE 100
 
+# Solving Diophantine Equation via https://www.alpertron.com.ar/QUAD.HTM gave this recurrence
+
 blue, n, L = 1, 1, 10**12
 while n <= L:
     blue, n = 3*blue + 2*n - 2, 4*blue + 3*n - 3
