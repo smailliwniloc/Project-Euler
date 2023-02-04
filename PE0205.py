0fd47e77b2177fa45db199351fc375447762c80f
Colin <smailliwniloc@gmail.com>
2023-02-03 20:02:26 -0600

    junk

--- PE0205.py
+++ PE0205.py
@@ -12,13 +12,3 @@ def roll(num, sides):
         ans += d(sides)
     return ans
 
-wins = 0
-totalRolls = 0
-maxRolls = 10000
-for i in range(maxRolls):
-    colin = roll(6, 6)
-    pete = roll(9, 4)
-    if(pete > colin):
-        wins += 1
-    totalRolls += 1
-    print(wins/totalRolls)
